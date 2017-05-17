10307e95af5ed9e88c336be734a1c8f90fd1f00a
Niyocchelas <Slinky7689@gmail.com>
2017-05-17 16:17:48 +0800

    Add files via upload

---
+++ ConverterApp/ConverterApp/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ConverterApp
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_UofM = new System.Windows.Forms.Label();
+            this.txt_UnitOfMeasure = new System.Windows.Forms.TextBox();
+            this.btn_CM_to_Inches = new System.Windows.Forms.Button();
+            this.btn_M_to_Feet = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.lbl_Display = new System.Windows.Forms.Label();
+            this.lbl_Convert = new System.Windows.Forms.Label();
+            this.txt_Convert = new System.Windows.Forms.TextBox();
+            this.C_to_F = new System.Windows.Forms.Button();
+            this.CM_to_FT = new System.Windows.Forms.Button();
+            this.KM_to_MI = new System.Windows.Forms.Button();
+            this.Results = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbl_UofM
+            // 
+            this.lbl_UofM.AutoSize = true;
+            this.lbl_UofM.Location = new System.Drawing.Point(9, 20);
+            this.lbl_UofM.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_UofM.Name = "lbl_UofM";
+            this.lbl_UofM.Size = new System.Drawing.Size(82, 13);
+            this.lbl_UofM.TabIndex = 0;
+            this.lbl_UofM.Text = "Unit of Measure";
+            // 
+            // txt_UnitOfMeasure
+            // 
+            this.txt_UnitOfMeasure.Location = new System.Drawing.Point(106, 17);
+            this.txt_UnitOfMeasure.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_UnitOfMeasure.Name = "txt_UnitOfMeasure";
+            this.txt_UnitOfMeasure.Size = new System.Drawing.Size(76, 20);
+            this.txt_UnitOfMeasure.TabIndex = 1;
+            // 
+            // btn_CM_to_Inches
+            // 
+            this.btn_CM_to_Inches.Location = new System.Drawing.Point(41, 50);
+            this.btn_CM_to_Inches.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_CM_to_Inches.Name = "btn_CM_to_Inches";
+            this.btn_CM_to_Inches.Size = new System.Drawing.Size(122, 19);
+            this.btn_CM_to_Inches.TabIndex = 2;
+            this.btn_CM_to_Inches.Text = "Centimetres to Inches";
+            this.btn_CM_to_Inches.UseVisualStyleBackColor = true;
+            this.btn_CM_to_Inches.Click += new System.EventHandler(this.btn_CM_to_Inches_Click);
+            // 
+            // btn_M_to_Feet
+            // 
+            this.btn_M_to_Feet.Location = new System.Drawing.Point(41, 84);
+            this.btn_M_to_Feet.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_M_to_Feet.Name = "btn_M_to_Feet";
+            this.btn_M_to_Feet.Size = new System.Drawing.Size(122, 19);
+            this.btn_M_to_Feet.TabIndex = 3;
+            this.btn_M_to_Feet.Text = "Metres to Feet";
+            this.btn_M_to_Feet.UseVisualStyleBackColor = true;
+            this.btn_M_to_Feet.Click += new System.EventHandler(this.btn_M_to_Feet_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(379, 11);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(56, 19);
+            this.btn_Exit.TabIndex = 4;
+            this.btn_Exit.Text = "EXIT";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // lbl_Display
+            // 
+            this.lbl_Display.AutoSize = true;
+            this.lbl_Display.Location = new System.Drawing.Point(7, 129);
+            this.lbl_Display.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Display.Name = "lbl_Display";
+            this.lbl_Display.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Display.TabIndex = 5;
+            // 
+            // lbl_Convert
+            // 
+            this.lbl_Convert.AutoSize = true;
+            this.lbl_Convert.Location = new System.Drawing.Point(212, 129);
+            this.lbl_Convert.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Convert.Name = "lbl_Convert";
+            this.lbl_Convert.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Convert.TabIndex = 6;
+            // 
+            // txt_Convert
+            // 
+            this.txt_Convert.Location = new System.Drawing.Point(152, 127);
+            this.txt_Convert.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Convert.Name = "txt_Convert";
+            this.txt_Convert.Size = new System.Drawing.Size(60, 20);
+            this.txt_Convert.TabIndex = 7;
+            // 
+            // C_to_F
+            // 
+            this.C_to_F.Location = new System.Drawing.Point(206, 50);
+            this.C_to_F.Name = "C_to_F";
+            this.C_to_F.Size = new System.Drawing.Size(116, 19);
+            this.C_to_F.TabIndex = 8;
+            this.C_to_F.Text = "Celcius to Fahrenheit";
+            this.C_to_F.UseVisualStyleBackColor = true;
+            this.C_to_F.Click += new System.EventHandler(this.C_to_F_Click);
+            // 
+            // CM_to_FT
+            // 
+            this.CM_to_FT.Location = new System.Drawing.Point(206, 84);
+            this.CM_to_FT.Name = "CM_to_FT";
+            this.CM_to_FT.Size = new System.Drawing.Size(116, 19);
+            this.CM_to_FT.TabIndex = 9;
+            this.CM_to_FT.Text = "Centimeters to Feet";
+            this.CM_to_FT.UseVisualStyleBackColor = true;
+            this.CM_to_FT.Click += new System.EventHandler(this.CM_to_FT_Click);
+            // 
+            // KM_to_MI
+            // 
+            this.KM_to_MI.Location = new System.Drawing.Point(352, 50);
+            this.KM_to_MI.Name = "KM_to_MI";
+            this.KM_to_MI.Size = new System.Drawing.Size(103, 19);
+            this.KM_to_MI.TabIndex = 10;
+            this.KM_to_MI.Text = "Kilometers to Miles";
+            this.KM_to_MI.UseVisualStyleBackColor = true;
+            this.KM_to_MI.Click += new System.EventHandler(this.KM_to_MI_Click);
+            // 
+            // Results
+            // 
+            this.Results.FormattingEnabled = true;
+            this.Results.Location = new System.Drawing.Point(344, 84);
+            this.Results.Name = "Results";
+            this.Results.Size = new System.Drawing.Size(120, 95);
+            this.Results.TabIndex = 11;
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 207);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.KM_to_MI);
+            this.Controls.Add(this.CM_to_FT);
+            this.Controls.Add(this.C_to_F);
+            this.Controls.Add(this.txt_Convert);
+            this.Controls.Add(this.lbl_Convert);
+            this.Controls.Add(this.lbl_Display);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_M_to_Feet);
+            this.Controls.Add(this.btn_CM_to_Inches);
+            this.Controls.Add(this.txt_UnitOfMeasure);
+            this.Controls.Add(this.lbl_UofM);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frm_Main";
+            this.Text = "ATCA Gas Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_UofM;
+        private System.Windows.Forms.TextBox txt_UnitOfMeasure;
+        private System.Windows.Forms.Button btn_CM_to_Inches;
+        private System.Windows.Forms.Button btn_M_to_Feet;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Label lbl_Display;
+        private System.Windows.Forms.Label lbl_Convert;
+        private System.Windows.Forms.TextBox txt_Convert;
+        private System.Windows.Forms.Button C_to_F;
+        private System.Windows.Forms.Button CM_to_FT;
+        private System.Windows.Forms.Button KM_to_MI;
+        private System.Windows.Forms.ListBox Results;
+    }
+}
+
---
+++ ConverterApp/ConverterApp/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConverterApp
+    // This program was written by Gail Mosdell
+    // It forms the base of a converter program for the OS-Assessment Two for Cert IV
+    // Date : February 2017
+    //Improved on by Nicholas Aylmore: April 2017
+{
+    public partial class frm_Main : Form
+    {
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        // Global Variables and Constants
+        double dbl_UofM, dbl_Convert;
+        double[] convertArray = new double[5];
+        int cnt = 0;
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //The code for calculating the Centermeters to Inches equation
+        private void btn_CM_to_Inches_Click(object sender, EventArgs e)
+        {
+
+            const double CM_TO_INCH = 0.3937;
+
+            if (!double.TryParse(txt_UnitOfMeasure.Text, out dbl_UofM))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure.Clear();
+                txt_UnitOfMeasure.Focus();
+                txt_Convert.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                if (cnt < 5)
+                {
+                    //This will display the results if more than 5 are entered and hold in an array in the 'Results' box
+                    dbl_Convert = dbl_UofM * CM_TO_INCH;
+                    txt_Convert.Text = dbl_Convert.ToString();
+                    convertArray[cnt] = dbl_Convert;
+                    cnt++;
+                    lbl_Display.Text = txt_UnitOfMeasure.Text + " centimetres is converted to ";
+                    lbl_Convert.Text = " inches.";
+                    //string[] items = Regex.Split(txt_Convert.Text + lbl_Convert.Text, "\r\n");
+                }
+                else
+                {
+                    for (int i = 0; i < cnt; i++)
+                    {
+                        Results.Items.Add(convertArray[i]);
+                    }
+                }
+                
+            }
+        }
+
+        //The code for calculating the Celcius to Fahrenheit equation
+        private void C_to_F_Click(object sender, EventArgs e)
+        {
+            const double C_TO_F = 1.8;
+                     
+            if (!double.TryParse(txt_UnitOfMeasure.Text, out dbl_UofM))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure.Clear();
+                txt_UnitOfMeasure.Focus();
+                txt_Convert.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                if (cnt < 5)
+                {
+                    //This will display the results if more than 5 are entered and hold in an array in the 'Results' box
+                    dbl_Convert = dbl_UofM * C_TO_F + 32;
+                    txt_Convert.Text = dbl_Convert.ToString();
+                    convertArray[cnt] = dbl_Convert;
+                    cnt++;
+                    lbl_Display.Text = txt_UnitOfMeasure.Text + " celcius is converted to ";
+                    lbl_Convert.Text = " fahrenheit.";
+                }
+                else
+                {
+                    for (int i = 0; i < cnt; i++)
+                    {
+                        Results.Items.Add(convertArray[i]);
+                    }
+                }
+            }
+        }
+
+        //The code for calculating the Centermeters to Feet equation
+        private void CM_to_FT_Click(object sender, EventArgs e)
+        {
+            const double CM_TO_FT = 0.0328084;
+                    
+
+            if (!double.TryParse(txt_UnitOfMeasure.Text, out dbl_UofM))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure.Clear();
+                txt_UnitOfMeasure.Focus();
+                txt_Convert.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                if (cnt < 5)
+                {
+                    //This will display the results if more than 5 are entered and hold in an array in the 'Results' box
+                    dbl_Convert = dbl_UofM * CM_TO_FT;
+                    txt_Convert.Text = dbl_Convert.ToString();
+                    convertArray[cnt] = dbl_Convert;
+                    cnt++;
+                    lbl_Display.Text = txt_UnitOfMeasure.Text + " Centimeters is converted to ";
+                    lbl_Convert.Text = " feet.";
+                }
+                else
+                {
+                    for (int i = 0; i < cnt; i++)
+                    {
+                        Results.Items.Add(convertArray[i]);
+                    }
+                }
+            }
+        }
+
+        //The code for calculating the Kilometers to Miles equation
+        private void KM_to_MI_Click(object sender, EventArgs e)
+        {
+            const double KM_TO_MI = 0.621371;
+                    
+
+            if (!double.TryParse(txt_UnitOfMeasure.Text, out dbl_UofM))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure.Clear();
+                txt_UnitOfMeasure.Focus();
+                txt_Convert.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                if (cnt < 5)
+                {
+                    //This will display the results if more than 5 are entered and hold in an array in the 'Results' box
+                    dbl_Convert = dbl_UofM * KM_TO_MI;
+                    txt_Convert.Text = dbl_Convert.ToString();
+                    lbl_Display.Text = txt_UnitOfMeasure.Text + " Kilometers is converted to ";
+                    lbl_Convert.Text = " Miles.";
+                }
+                else
+                {
+                    for (int i = 0; i < cnt; i++)
+                    {
+                        Results.Items.Add(convertArray[i]);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //the code for calculating the Meters to Feet equation
+        private void btn_M_to_Feet_Click(object sender, EventArgs e)
+        {
+            const double M_TO_FT = 3.28084;
+
+          
+            if (!double.TryParse(txt_UnitOfMeasure.Text, out dbl_UofM))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure.Clear();
+                txt_UnitOfMeasure.Focus();
+                txt_Convert.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                if (cnt < 5)
+                {
+                    //This will display the results if more than 5 are entered and hold in an array in the 'Results' box
+                    dbl_Convert = dbl_UofM * M_TO_FT;
+                    txt_Convert.Text = dbl_Convert.ToString();
+                    lbl_Display.Text = txt_UnitOfMeasure.Text + " meters is converted to ";
+                    lbl_Convert.Text = " feet.";                   
+                }
+                else
+                {
+                    for (int i = 0; i < cnt; i++)
+                    {
+                        Results.Items.Add(convertArray[i]);
+                    }
+                }
+            }
+        }
+
+    }
+}
